c53a2739405b9002846063cba2af4b4ad977ebd6
Toan Ho <toanbku@gmail.com>
2023-02-16 09:28:19 +0700

    chore: update found match

--- components/MatchMaking/FoundMatch.tsx
+++ components/MatchMaking/FoundMatch.tsx
@@ -4,9 +4,9 @@ export const FoundMatch = () => {
   return (
     <div className="w-full h-full fixed bg-black/90 top-0 left-0 z-50 flex justify-center items-center">
       <div className="relative w-96 h-96 p-6 text-white border-solid border-2 border-yellow-800 rounded-full">
-        <div className="w-full h-full rounded-full border border-yellow-300">
+        <div className="w-full h-full rounded-full border border-yellow-300 bg-gray-800">
           <div className="w-full h-full rounded-full border-[10px] border-blue-400 flex flex-col justify-center text-center">
-            <div className="relative w-full h-full p-8 rounded-full border border-yellow-300">
+            <div className="relative w-full h-full p-8 rounded-full border border-yellow-300 flex flex-col justify-center">
               <div className="font-bold text-yellow-200 text-2xl">
                 Match Found
               </div>
@@ -14,7 +14,7 @@ export const FoundMatch = () => {
                 Summoner&#39;s Rift • Co-op vs. AI • 5v5
               </div>
 
-              <div className="bg-black absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 border border-yellow-300 p-1">
+              <div className="accept-button bg-black absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 border border-yellow-300 p-1">
                 <button className="py-1 px-4 border border-cyan-500 bg-slate-700">
                   Accept!
                 </button>
